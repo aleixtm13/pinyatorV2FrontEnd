a3c27e38ce9fbe565fdd9db1f0116cce5b591110
beri <NoOPeEKS>
2023-02-04 00:20:03 +0100

    Added non-functional add casteller button

--- pinyatorv2frontend/src/pages/Castellers.jsx
+++ pinyatorv2frontend/src/pages/Castellers.jsx
@@ -7,6 +7,9 @@ const Castellers = () => {
   {/* Ha de mostrar una taula amb els atributs importants de tots els castellers de la base de dades */}
   return (
     <div>
+      <button className="m-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+        Nou Casteller
+      </button>
       <TaulaCastellers/>
     </div>
   )
--- pinyatorv2frontend/src/pages/Home.jsx
+++ pinyatorv2frontend/src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-white 2xl font-bold'>Home</div>
+    <div className='flex justify-center items-center'>
+        <h1 className='5xl font-bold underline underline-offset-2 text-white'>Benvinguts al PinyatorV2</h1>
+    </div>
   )
 }
 
